a04d6a954cbf8e110d93dd20b48e0b0bb6d57c7f
Issei-Sato-0926 <satoon.0926@outlook.jp>
2023-12-26 21:16:58 +0900

    Hello World をレスポンスする処理の作成

--- src/main/java/com/sato/HelloWorld/HelloWorldController.java
+++ src/main/java/com/sato/HelloWorld/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.sato.HelloWorld;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class HelloWorldController {
+    @GetMapping("/HelloWorld")
+    public String HelloWorld() {
+        return "Hello World";
+    }
 }
